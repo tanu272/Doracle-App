7ab229842e1fbbe003563bbfa624e9dea9821870
Tanushree Bhattacharya <55945733+tanu272@users.noreply.github.com>
2021-05-25 02:00:59 +0530

    Update App.js

--- p/App.js
+++ p/App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import { NavigationContainer, DrawerActions } from '@react-navigation/native';
 import {
   createDrawerNavigator,
@@ -14,11 +14,35 @@ function CustomDrawerContent(props) {
   return (
     <DrawerContentScrollView {...props}>
       <DrawerItemList {...props} />
-      <DrawerItem
-        label="Close Menu"
-        onPress={() => props.navigation.dispatch(DrawerActions.closeDrawer())}
-      />
     </DrawerContentScrollView>
+  );
+}
+
+function Logout({navigation})
+{
+  return (
+    <View style= {styles.MainContainer}>
+      <TouchableOpacity style={styles.FacebookStyle} 
+      onPress = {() => navigation.dispatch(DrawerActions.openDrawer())}
+      activeOpacity={0.1}>
+ 
+         <Image 
+          source={require('./hmb.png')} 
+          style={styles.ImageIconStyle} 
+          />
+       </TouchableOpacity>
+        <Text style={{color:"white", fontSize:25, alignContent:'center', paddingTop: 200}}>
+        Are you sure you wanna logout?
+        <Text>{"\n"}</Text>
+        </Text>
+        <Button
+        title ="YES"
+        />
+        <Text>{"\n"}</Text>
+        <Button
+        title ="NO"
+        />
+     </View>
   );
 }
 
@@ -29,6 +53,7 @@ function MyDrawer() {
     <Drawer.Navigator drawerContent={props => <CustomDrawerContent {...props} />}>
       <Drawer.Screen name="Home" component={Home} />
       <Drawer.Screen name="Medical Records" component={MedicalRecords} />
+      <Drawer.Screen name="Logout" component={Logout} />
     </Drawer.Navigator>
   );
 }
@@ -40,3 +65,36 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+
+  MainContainer: {
+   flex:1, 
+   paddingTop: 40, 
+   paddingLeft: 10, 
+   paddingRight: 10, 
+   backgroundColor: '#003f5c',
+ },
+
+  FacebookStyle: {
+  flexDirection: 'row',
+  alignItems: 'center',
+  backgroundColor: '#003f5c',
+  borderWidth: .5,
+  borderColor: '#003f5c',
+  height: 40,
+  borderRadius: 5 ,
+  margin: 5,
+ 
+},
+ 
+ImageIconStyle: {
+   padding: 10,
+   margin: 5,
+   height: 25,
+   width: 25,
+   resizeMode : 'stretch',
+ 
+},
+});
+
