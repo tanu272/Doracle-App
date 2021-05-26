84c9ae6fe4d8a4be5217dfffa99abf86e95e20af
Tanushree Bhattacharya <55945733+tanu272@users.noreply.github.com>
2021-05-27 03:12:04 +0530

    Added Lab Reports and Pharmacy Requirements

--- p/App.js
+++ p/App.js
@@ -1,19 +1,25 @@
 import * as React from 'react';
-import { View, Text, Button, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import { View, Text, Button, TouchableOpacity, StyleSheet, Image, Linking, Platform } from 'react-native';
 import { NavigationContainer, DrawerActions } from '@react-navigation/native';
 import {
   createDrawerNavigator,
   DrawerContentScrollView,
   DrawerItemList,
   DrawerItem,
+  DrawerContent
 } from '@react-navigation/drawer';
+import { useWindowDimensions } from 'react-native';
 import Home from './Home';
 import MedicalRecords from './MedicalRecords'
+import LabReports from './LabReport'
+import Req from './Requirements'
+import Icon from 'react-native-vector-icons/Ionicons';
 
 function CustomDrawerContent(props) {
   return (
     <DrawerContentScrollView {...props}>
       <DrawerItemList {...props} />
+      
     </DrawerContentScrollView>
   );
 }
@@ -31,7 +37,7 @@ function Logout({navigation})
           style={styles.ImageIconStyle} 
           />
        </TouchableOpacity>
-        <Text style={{color:"white", fontSize:25, alignContent:'center', paddingTop: 200}}>
+        <Text style={{color:"#f0b30c", fontSize:25, alignContent:'center', paddingTop: 200}}>
         Are you sure you wanna logout?
         <Text>{"\n"}</Text>
         </Text>
@@ -48,12 +54,76 @@ function Logout({navigation})
 
 const Drawer = createDrawerNavigator();
 
+
+
 function MyDrawer() {
+  const dimensions = useWindowDimensions();
+  const isLargeScreen = dimensions.width >= 768;
   return ( 
-    <Drawer.Navigator drawerContent={props => <CustomDrawerContent {...props} />}>
-      <Drawer.Screen name="Home" component={Home} />
-      <Drawer.Screen name="Medical Records" component={MedicalRecords} />
-      <Drawer.Screen name="Logout" component={Logout} />
+    <Drawer.Navigator drawerType={dimensions.width >= 768 ? 'permanent' : 'front'}
+    drawerStyle={{
+      backgroundColor: '#ffd5ab',
+      activeOpacity: 0.3,
+      width: 275}}
+      drawerContentOptions={{
+        activeTintColor: '#003f5c',
+        itemStyle: { marginTop : 50, paddingTop: 10, paddingBottom: 10,},
+        alignContent: 'center',
+      }}
+      drawerContent={props => <CustomDrawerContent {...props} />}>
+      <Drawer.Screen name="Home" component={Home} 
+      options={{ drawerLabel: 'Home', 
+                 drawerIcon: config => <Icon
+                size={23}
+                color="#000000"
+                name={Platform.OS === 'android' ? 'md-home' : 'ios-home'}></Icon> ,  
+              }}
+      />
+      <Drawer.Screen name="Medical Records" 
+      component={MedicalRecords} 
+      options={{ drawerLabel: 'Medical Records', 
+                 drawerIcon: config => <Icon
+                size={23}
+                color="#000000"
+                name={Platform.OS === 'android' ? 'md-clipboard' : 'ios-clipboard'}></Icon> ,
+      }}
+      />
+      <Drawer.Screen name="Pharmacy" 
+      component={Req} 
+      options={{ drawerLabel: 'Pharmacy Requirements', 
+                 drawerIcon: config => <Icon
+                size={23}
+                color="#000000"
+                name={Platform.OS === 'android' ? 'md-egg' : 'ios-egg'}></Icon> ,
+      }}
+      />
+      <Drawer.Screen name="Lab Reports" 
+      component={LabReports} 
+      options={{ drawerLabel: 'Lab Reports', 
+                 drawerIcon: config => <Icon
+                size={23}
+                color="#000000"
+                name={Platform.OS === 'android' ? 'md-document' : 'ios-document'}></Icon> ,
+      }}
+      />
+      <Drawer.Screen
+        name="Help"
+         options={{ drawerLabel: 'Complaint', 
+                 drawerIcon: config => <Icon
+                size={23}
+                color="#000000"
+                name={Platform.OS === 'android' ? 'md-help-circle' : 'ios-help-circle'}></Icon> ,
+      }}
+        onPress={() => Linking.openURL('https://mywebsite.com/help')}
+      />
+      <Drawer.Screen name="Logout" component={Logout} 
+       options={{ drawerLabel: 'Logout', 
+                 drawerIcon: config => <Icon
+                size={23}
+                color="#000000"
+                name={Platform.OS === 'android' ? 'md-log-out' : 'ios-log-out'}></Icon> ,
+      }}
+      />
     </Drawer.Navigator>
   );
 }
@@ -97,4 +167,3 @@ ImageIconStyle: {
  
 },
 });
-
